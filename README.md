f951b1f250d448ea569d885285a3de876211e24f
xmdi <xmdi@protonmail.com>
2017-08-22 22:58:01 -0400

    clean up and proxies

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # BRB 
 Bing Rewards Bot - this python script earns Microsoft Rewards points for searches.
 
-Dependencies: Selenium, chromedriver (add to path), CSV of login credentials in credentials.txt (email, password).
+Dependencies: `Selenium, chromedriver`
 
-TO DO: proxies
+It uses a CSV of login credentials in credentials.txt (email, password).
+
+Usage: `python main.py` 
+
+Proxies technically supported with `-p` but Microsoft often requires authentication for unfamiliar IPs.
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.common.exceptions import StaleElementReferenceException
 import time
 from random import randint
+import sys
 
 def waitel(b,xpath):
     while(True):
@@ -46,26 +47,50 @@ def doSearches(b,number,em,pw):
 def getPoints(b,em):
     global cv
     b.get('https://account.microsoft.com/rewards')
-    q=int(b.find_element_by_xpath('//*[@id="dashboard"]/div[2]/div[1]/div/div[1]').text.replace(',',''))
+    q=int(b.find_element_by_xpath('//*[@id="dashboard"]/div[2]/div[1]/div/div[1]').text.replace(',','')) 
     print(em[:10],'...','%8s'%str(q),'pts,','$%.2f'%(q/5250))
     cv+=q
 
-def mainloop(em,pw):
-    b = webdriver.Chrome() 
+def mainloop(em,pw,PROXY):
+    op=webdriver.ChromeOptions()
+    if po:
+        op.add_argument('--proxy-server=%s'%PROXY)
+    b = webdriver.Chrome(chrome_options=op) 
     doSearches(b,30,em,pw) # pc searches
-    capabilities={'chromeOptions':{'mobileEmulation':{'deviceName':'Nexus 7'}}}
+    capabilities={'chromeOptions':{'mobileEmulation':{'deviceName':'Galaxy S III'}}}
     b.start_session(capabilities) 
     doSearches(b,20,em,pw) # mobile searches
     getPoints(b,em)
     b.quit()
 
+def scrapeProxies(qty):
+    proxies=[]
+    b=webdriver.Chrome()
+    b.get('http://www.us-proxy.org')
+    b.find_element_by_xpath('//*[@id="proxylisttable"]/thead/tr/th[7]').click()
+    b.find_element_by_xpath('//*[@id="proxylisttable"]/thead/tr/th[7]').click()
+    time.sleep(1)
+    for q in range(0,qty):
+        proxies.append(b.find_element_by_xpath('//*[@id="proxylisttable"]/tbody/tr['+str(q+1)+']/td[1]').text+':'+b.find_element_by_xpath('//*[@id="proxylisttable"]/tbody/tr['+str(q+1)+']/td[2]').text)
+    b.quit()
+    print(qty,'proxies scraped from us-proxy.org')
+    return proxies
+
 t0=time.time()
+po=0
+if len(sys.argv)>1:
+    if sys.argv[1]=='-p':
+        po=1
 num_lines = sum(1 for line in open('words.txt'))
+num_accts = sum(1 for line in open('credentials.txt'))
+PROXIES=[0]*num_accts
+if po:
+    PROXIES=scrapeProxies(num_accts)
 cv=0
 na=0
 with open('credentials.txt','r+') as f:
     for elem in f.readlines():
-        mainloop(elem.split(',')[0].strip(),elem.split(',')[1].strip())
+        mainloop(elem.split(',')[0].strip(),elem.split(',')[1].strip(),PROXIES[na])
         na+=1
 print('TOTALS .......','%8s'%str(cv),'pts,','$%.2f'%(cv/5250))
 print(na,'account(s) completed in',time.time()-t0,'seconds.')
